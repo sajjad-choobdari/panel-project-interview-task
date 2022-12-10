890d8caca04b62efdd269df3911a774da8c6e5d5
Sajjad Choobdari <sjd.ch78@gmail.com>
2022-12-10 18:42:52 +0330

    data grid demo added

--- src/routes/data-grid-demo.jsx
+++ src/routes/data-grid-demo.jsx
@@ -1,11 +1,58 @@
-import { Stack } from '@mui/material';
+import { Paper, Stack } from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import { useMemo } from 'react';
 
-function DataGridDemoPage() {
+const VISIBLE_FIELDS = [
+  'traderName',
+  'traderEmail',
+  'quantity',
+  'filledQuantity',
+  'isFilled',
+  'status',
+  'incoTerm',
+  'pnl',
+  'counterPartyCountry',
+  'dateCreated'
+];
+
+
+export default function DataGridDemoPage() {
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100000,
+    editable: true,
+  });
+
+  const columns = useMemo(
+    () => data.columns.filter((column) => VISIBLE_FIELDS.includes(column.field)),
+    [data.columns],
+  );
+
   return (
     <Stack direction="column" alignItems="center" height="100%">
-      <p>Hello World</p>
+      <Paper sx={{ width: 'calc(100% - 3vw)', height: '100%', margin: 3 }} elevation={2}>
+        {/* <Typography variant="h4" component="h1" gutterBottom>All-in-one Table!</Typography> */}
+        <DataGrid
+          {...data}
+          columns={columns}
+          loading={data.rows.length === 0}
+          checkboxSelection
+          disableSelectionOnClick
+          experimentalFeatures={{ newEditingApi: true }}
+          components={{ Toolbar: GridToolbar }}
+          componentsProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
+          }}
+          // disableColumnFilter
+          // disableColumnSelector
+          // disableDensitySelector
+        />
+      </Paper>
     </Stack>
   );
 }
-
-export { DataGridDemoPage };
+export { DataGridDemoPage }
